0f546ac365e03a61adbdd1032b64aba7c4236f66
Shiva <manas051@gmail.com>
2024-12-12 16:02:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -124,3 +124,56 @@ $(document).ready(function () {
 }); 
 </script>
 
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail/dot controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    
+    if (n > slides.length) { slideIndex = 1; }
+    if (n < 1) { slideIndex = slides.length; }
+
+    // Hide all slides
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    // Remove 'active' class from all dots
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    // Show the current slide and set active dot
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+// Automatic slideshow
+let autoIndex = 0;
+autoSlides();
+
+function autoSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    autoIndex++;
+    if (autoIndex > slides.length) { autoIndex = 1; }
+    slides[autoIndex - 1].style.display = "block";
+    setTimeout(autoSlides, 3000); // Change image every 3 seconds
+}
+
+
